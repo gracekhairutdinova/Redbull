165c2a09bfd5eac73280722ebee777ec560d273b
Grace <126357549+gracekhairutdinova@users.noreply.github.com>
2023-12-07 19:28:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".my-slider").bxSlider();
+  });
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    slideWidth: 150,
+    minSlides: 5,
+    maxSlides: 6,
+    // slideMargin: 20
+  });
+});
